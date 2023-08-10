c2c39d68962189f030ff69b13dbcee04745f8aa0
Adam Smelko <adamsmelko1@gmail.com>
2023-08-10 12:22:59 +0200

    add timing prints

--- src/simulator.cpp
+++ src/simulator.cpp
@@ -12,8 +12,101 @@
 #include "original/modules/various_output.h"
 #include "original/standard_models.h"
 
+#define measure(F, D)                                                                                                  \
+	durations_.start = std::chrono::high_resolution_clock::now();                                                      \
+	F;                                                                                                                 \
+	durations_.end = std::chrono::high_resolution_clock::now();                                                        \
+	D += std::chrono::duration_cast<std::chrono::microseconds>(durations_.end - durations_.start).count();
+
+#define make_duration_pair(D) std::make_pair(D, #D)
+
 using namespace biofvm;
 using namespace physicell;
+
+void simulator_durations::print_durations()
+{
+	std::array<std::pair<std::size_t, const char*>, 20> durations { make_duration_pair(diffusion),
+																	make_duration_pair(secretion),
+																	make_duration_pair(gradient),
+																	make_duration_pair(custom_rules),
+																	make_duration_pair(custom_interactions),
+																	make_duration_pair(forces),
+																	make_duration_pair(motility),
+																	make_duration_pair(membrane),
+																	make_duration_pair(spring),
+																	make_duration_pair(position),
+																	make_duration_pair(cell_cell),
+																	make_duration_pair(container_mech),
+																	make_duration_pair(mesh_mech),
+																	make_duration_pair(neighbors_mech),
+																	make_duration_pair(advance_phe),
+																	make_duration_pair(container_phe),
+																	make_duration_pair(mesh_phe),
+																	make_duration_pair(neighbors_phe),
+																	make_duration_pair(svg_save),
+																	make_duration_pair(full_save) };
+
+	std::size_t diff_total = 0;
+	std::size_t mech_total = 0;
+	std::size_t phen_total = 0;
+	std::size_t save_total = 0;
+	std::size_t total = 0;
+
+	{
+		index_t i = 0;
+		for (; i < 2; i++)
+			diff_total += durations[i].first;
+
+		for (; i < 14; i++)
+			mech_total += durations[i].first;
+
+		for (; i < 18; i++)
+			phen_total += durations[i].first;
+
+		for (; i < 20; i++)
+			save_total += durations[i].first;
+
+		total = diff_total + mech_total + phen_total + save_total;
+	}
+
+	std::cout << "Duration ratios: "
+			  << "Diffusion: " << (int)(diff_total / (double)total * 100)
+			  << "% Mechanics: " << (int)(mech_total / (double)total * 100)
+			  << "% Phenotype: " << (int)(phen_total / (double)total * 100)
+			  << "% Save: " << (int)(save_total / (double)total * 100) << "%" << std::endl;
+
+	std::sort(durations.begin(), durations.end(), [](auto& a, auto& b) { return a.first > b.first; });
+
+	std::cout << "  ";
+	for (index_t i = 0; i < 5; i++)
+	{
+		std::cout << durations[i].second << ": " << durations[i].first / 1000. << "ms ";
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	// zero them out
+	diffusion = 0;
+	secretion = 0;
+	gradient = 0;
+	custom_rules = 0;
+	custom_interactions = 0;
+	forces = 0;
+	motility = 0;
+	membrane = 0;
+	spring = 0;
+	position = 0;
+	cell_cell = 0;
+	container_mech = 0;
+	mesh_mech = 0;
+	neighbors_mech = 0;
+	advance_phe = 0;
+	container_phe = 0;
+	mesh_phe = 0;
+	neighbors_phe = 0;
+	svg_save = 0;
+	full_save = 0;
+}
 
 void simulator::initialize(environment& e, PhysiCell_Settings& settings)
 {
@@ -50,10 +143,11 @@ void simulator::simulate_diffusion_and_mechanics(environment& e)
 {
 	{
 		// Compute diffusion:
-		diffusion_solver_.diffusion.solve(e.m);
+		measure(diffusion_solver_.diffusion.solve(e.m), durations_.diffusion);
 
 		// Compute secretion and uptake:
-		diffusion_solver_.cell.simulate_secretion_and_uptake(e.m, recompute_secretion_and_uptake_);
+		measure(diffusion_solver_.cell.simulate_secretion_and_uptake(e.m, recompute_secretion_and_uptake_),
+				durations_.secretion);
 
 		recompute_secretion_and_uptake_ = false;
 	}
@@ -61,36 +155,36 @@ void simulator::simulate_diffusion_and_mechanics(environment& e)
 	if (simulation_step_ % mechanics_step_interval_ == 0)
 	{
 		// Compute gradient:
-		diffusion_solver_.gradient.solve(e.m);
+		measure(diffusion_solver_.gradient.solve(e.m), durations_.gradient);
 
 		// custom cell rules:
-		custom_cell_rules(e);
+		measure(custom_cell_rules(e), durations_.custom_rules);
 
 		// Compute velocities and update the positions:
 		{
 			// custom attached cells adhesion:
-			evaluate_interactions(e);
+			measure(evaluate_interactions(e), durations_.custom_interactions);
 
-			mechanics_solver_.position.update_cell_forces(e);
-			mechanics_solver_.position.update_motility(e);
-			mechanics_solver_.position.update_basement_membrane_interactions(e);
-			mechanics_solver_.position.update_spring_attachments(e);
-			mechanics_solver_.position.update_positions(e);
+			measure(mechanics_solver_.position.update_cell_forces(e), durations_.forces);
+			measure(mechanics_solver_.position.update_motility(e), durations_.motility);
+			measure(mechanics_solver_.position.update_basement_membrane_interactions(e), durations_.membrane);
+			measure(mechanics_solver_.position.update_spring_attachments(e), durations_.spring);
+			measure(mechanics_solver_.position.update_positions(e), durations_.position);
 		}
 
 		// Standard cell-cell interactions:
-		mechanics_solver_.interactions.update_cell_cell_interactions(e);
+		measure(mechanics_solver_.interactions.update_cell_cell_interactions(e), durations_.cell_cell);
 
 		// housekeeping
 		{
 			// Removal of flagged cells from data structures:
-			mechanics_solver_.containers.update_cell_container_for_mechanics(e);
+			measure(mechanics_solver_.containers.update_cell_container_for_mechanics(e), durations_.container_mech);
 
 			// Update mechanics mesh with new cell positions:
-			mechanics_solver_.containers.update_mechanics_mesh(e);
+			measure(mechanics_solver_.containers.update_mechanics_mesh(e), durations_.mesh_mech);
 
 			// Update cells neighbors:
-			mechanics_solver_.position.update_cell_neighbors(e);
+			measure(mechanics_solver_.position.update_cell_neighbors(e), durations_.neighbors_mech);
 
 			recompute_secretion_and_uptake_ = true;
 		}
@@ -99,21 +193,47 @@ void simulator::simulate_diffusion_and_mechanics(environment& e)
 	if (simulation_step_ % phenotype_step_interval_ == 0)
 	{
 		// Update phenotype:
-		advance_bundled_phenotype_functions(e);
+		measure(advance_bundled_phenotype_functions(e), durations_.advance_phe);
 
 		// housekeeping
 		{
 			// Removal and division of flagged cells in data structures:
-			mechanics_solver_.containers.update_cell_container_for_phenotype(e, diffusion_solver_.cell);
+			measure(mechanics_solver_.containers.update_cell_container_for_phenotype(e, diffusion_solver_.cell),
+					durations_.container_phe);
 
 			// Update mechanics mesh with new cell positions:
-			mechanics_solver_.containers.update_mechanics_mesh(e);
+			measure(mechanics_solver_.containers.update_mechanics_mesh(e), durations_.mesh_phe);
 
 			// Update cells neighbors:
-			mechanics_solver_.position.update_cell_neighbors(e);
+			measure(mechanics_solver_.position.update_cell_neighbors(e), durations_.neighbors_phe);
 
 			recompute_secretion_and_uptake_ = true;
 		}
+	}
+}
+
+void simulator::save_full(environment& e, PhysiCell_Settings& settings)
+{
+	if (settings.enable_full_saves == true)
+	{
+		std::stringstream ss;
+		ss << settings.folder << "/output" << std::setfill('0') << std::setw(8)
+		   << simulation_step_ / full_save_interval_;
+
+		save_PhysiCell_to_MultiCellDS_v2(ss.str(), e);
+	}
+}
+
+void simulator::save_svg(environment& e, PhysiCell_Settings& settings,
+						 const cell_coloring_funct_t& cell_coloring_function)
+{
+	if (settings.enable_SVG_saves == true)
+	{
+		std::stringstream ss;
+		ss << settings.folder << "/snapshot" << std::setfill('0') << std::setw(8)
+		   << simulation_step_ / svg_save_interval_ << ".svg";
+
+		SVG_plot(ss.str(), e, settings, 0.0, e.current_time, cell_coloring_function);
 	}
 }
 
@@ -148,36 +268,22 @@ void simulator::run(environment& e, PhysiCell_Settings& settings, cell_coloring_
 
 	while (e.current_time < settings.max_time + 0.1 * e.m.diffusion_time_step)
 	{
-		// save data if it's time.
-		if (simulation_step_ % full_save_interval_ == 0)
-		{
-			display_simulation_status(std::cout, e, settings);
-
-			if (settings.enable_full_saves == true)
-			{
-				std::stringstream ss;
-				ss << settings.folder << "/output" << std::setfill('0') << std::setw(8)
-				   << simulation_step_ / full_save_interval_;
-
-				save_PhysiCell_to_MultiCellDS_v2(ss.str(), e);
-			}
-		}
-
 		// save SVG plot if it's time
 		if (simulation_step_ % svg_save_interval_ == 0)
 		{
-			if (settings.enable_SVG_saves == true)
-			{
-				std::stringstream ss;
-				ss << settings.folder << "/snapshot" << std::setfill('0') << std::setw(8)
-				   << simulation_step_ / svg_save_interval_ << ".svg";
+			measure(save_svg(e, settings, cell_coloring_function), durations_.svg_save);
+		}
 
-				SVG_plot(ss.str(), e, settings, 0.0, e.current_time, cell_coloring_function);
-			}
+		// save data if it's time.
+		if (simulation_step_ % full_save_interval_ == 0)
+		{
+			measure(save_full(e, settings), durations_.full_save);
+
+			display_simulation_status(std::cout, e, settings);
+			durations_.print_durations();
 		}
 
 		simulate_diffusion_and_mechanics(e);
-
 		e.current_time += e.m.diffusion_time_step;
 		++simulation_step_;
 	}
--- src/simulator.h
+++ src/simulator.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <chrono>
+
 #include <BioFVM/solver.h>
 
 #include "environment.h"
@@ -9,8 +11,38 @@
 
 namespace physicell {
 
+struct simulator_durations
+{
+	std::chrono::high_resolution_clock::time_point start, end;
+
+	std::size_t diffusion = 0;
+	std::size_t secretion = 0;
+	std::size_t gradient = 0;
+	std::size_t custom_rules = 0;
+	std::size_t custom_interactions = 0;
+	std::size_t forces = 0;
+	std::size_t motility = 0;
+	std::size_t membrane = 0;
+	std::size_t spring = 0;
+	std::size_t position = 0;
+	std::size_t cell_cell = 0;
+	std::size_t container_mech = 0;
+	std::size_t mesh_mech = 0;
+	std::size_t neighbors_mech = 0;
+	std::size_t advance_phe = 0;
+	std::size_t container_phe = 0;
+	std::size_t mesh_phe = 0;
+	std::size_t neighbors_phe = 0;
+	std::size_t full_save = 0;
+	std::size_t svg_save = 0;
+
+	void print_durations();
+};
+
 class simulator
 {
+	simulator_durations durations_;
+
 	biofvm::solver diffusion_solver_;
 	physicell::solver mechanics_solver_;
 
@@ -22,6 +54,9 @@ class simulator
 	biofvm::index_t svg_save_interval_;
 
 	bool recompute_secretion_and_uptake_;
+
+	void save_full(environment& e, PhysiCell_Settings& settings);
+	void save_svg(environment& e, PhysiCell_Settings& settings, const cell_coloring_funct_t& cell_coloring_function);
 
 public:
 	void initialize(environment& e, PhysiCell_Settings& settings);
